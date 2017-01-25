1652d45c21fbc9bcefe0cde79252422085f27fe0
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-25 10:15:34 -0800

    add function to hold. Saves and display current total score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 //business logic
 var roundTotal = 0;
+var totalScore = 0;
 function roll() {
   var currentRoll = Math.ceil(Math.random() * 6);
   if (currentRoll === 1) {
-    roundTotal === 0;
+    roundTotal = 0;
   } else {
     roundTotal += currentRoll;
   }
   return currentRoll;
+}
+
+function hold() {
+  totalScore += roundTotal;
+  roundTotal = 0;
+  return totalScore;
 }
 
 
@@ -16,8 +23,13 @@ function roll() {
 $(document).ready(function() {
 
   $("#roll").click(function() {
-    var output=roll()
-    $("#last-roll").text(output);
+    var thisRoll = roll();
+    $("#last-roll").text(thisRoll);
+    $("#round-total").text(roundTotal);
+  })
+  $("#hold").click(function(){
+    var total = hold();
+    $("#total-score").text(total);
     $("#round-total").text(roundTotal);
   })
 })
