e18cd85294fa8999a1640fdf25d693cfed17ce6a
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-25 14:39:49 -0800

    add ability to store both player scores and check for victory

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
 //business logic
-var roundTotal = 0;
-var totalScore = 0;
-var winningScore = 10;
-
-function checkVictory() {
-  if (totalScore + roundTotal >= winningScore) {
-    $("#win").text("Congrats, you won!");
-  }
-}
 
 function Player(name) {
   this.name = name;
@@ -22,56 +13,69 @@ function Game(winningscore, maxrounds, players) {
   this.roundTotal = 0;
 }
 
-Game.protoype = {
-  switchPlayer: function() {
+Game.prototype.switchPlayer = function() {
     if (this.currentplayer === this.players[0]) {
       this.currentplayer = this.players[1];
     } else if (this.currentplayer === this.players[1]) {
       this.currentplayer = this.players[0];
     }
-  },
-  hold: function() {
-    totalScore += roundTotal;
-    roundTotal = 0;
+    $("#current-player").text(this.currentplayer.name);
+}
+Game.prototype.hold = function() {
+  this.currentplayer.score += this.roundTotal;
+  this.roundTotal = 0;
+  this.switchPlayer();
+}
+Game.prototype.roll = function() {
+  var currentRoll = Math.ceil(Math.random() * 6);
+  if (currentRoll === 1) {
+    this.roundTotal = 0;
     this.switchPlayer();
-    return totalScore;
-  },
-  roll: function() {
-    var currentRoll = Math.ceil(Math.random() * 6);
-    if (currentRoll === 1) {
-      roundTotal = 0;
-      this.switchPlayer();
-    } else {
-      roundTotal += currentRoll;
-    }
-    return currentRoll;
+  } else {
+    this.roundTotal += currentRoll;
+  }
+  return currentRoll;
+}
+Game.prototype.checkVictory = function() {
+  if (this.currentplayer.score + this.roundTotal >= this.winningscore) {
+    this.currentplayer.score += this.roundTotal;
+    $("#player1Score").text(this.players[0].score);
+    $("#player2Score").text(this.players[1].score);
+    $("#win").text("Congrats, you won!");
   }
 }
+// };
 
 //user interface logic
 $(document).ready(function() {
-$("form#names").submit(function() {
-    var inputtedName1 = $("input:name1").val();
-    var inputtedName2 = $("input:name2").val();
+  $("form#names").submit(function(event) {
+    event.preventDefault();
+    var inputtedName1 = $("input[name=name1]").val();
+    var inputtedName2 = $("input[name=name2]").val();
+    var winningScore = 10;//make it take user input later
 
     var player1 = new Player(inputtedName1);
     var player2 = new Player(inputtedName2);
 
     var game = new Game(winningScore, 1000, [player1, player2]);
 
+    $("#player1").text(inputtedName1);
+    $("#player2").text(inputtedName2);
+
+    $("#current-player").text(game.currentplayer.name);
+
     $("#roll").click(function() {
       var thisRoll = game.roll();
+
       $("#last-roll").text(thisRoll);
-      $("#round-total").text(roundTotal);
-      checkVictory();
+      $("#round-total").text(game.roundTotal);
+
+      game.checkVictory();
     })
     $("#hold").click(function(){
-      var total = game.hold();
-      $("#total-score").text(total);
-      $("#round-total").text(roundTotal);
+      game.hold();
+      $("#player1Score").text(player1.score);
+      $("#player2Score").text(player2.score);
     })
   })
-
-
-
 })
